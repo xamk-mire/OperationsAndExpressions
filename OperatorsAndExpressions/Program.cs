e2a2872a1265569fc34296df5a3cf3a4a7c1cee6
Miika Reijonen <hmire02@xamk.fi>
2023-09-25 16:41:22 +0300

    Add exersices

--- OperatorsAndExpressions/Program.cs
+++ OperatorsAndExpressions/Program.cs
@@ -1,10 +1,37 @@
-﻿namespace OperatorsAndExpressions
+﻿using System.Diagnostics.Metrics;
+
+namespace OperatorsAndExpressions
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Task 1: Arithmetic Operators
+            // - Prompts the user to enter two numbers
+            // - Computes and siplays the addition, subtraction, multiplication, division and modulus
+            //   of the two numbers
+            // Enter your code below
+
+            // Task 2: Relational and Equality Operators
+            // - Prompts the user to enter two integers
+            // - Compares the two integers and displays whether they are equal, and if not, which one is larger.
+            // Enter your code below
+
+            // Task 3: Logical Operators
+            // - Prompts the user to enter an age.
+            // - Checks if the age is greater than 13 and less than 20.If it is, print "Teenager".Otherwise, print "Not a teenager".
+            // Enter your code below
+
+            // Task 4: Unary and Assignment Operators
+            // - Initializes a variable called counter with a value of 10.
+            // - Increments the counter using the ++ operator.
+            // - Decrements the counter using the -- operator.
+            // - Multiplies the counter by 5 using the *= operator.
+            // - Divides the counter by 2 using the /= operator.
+            // - Displays the current value of counter after each operation.
+            // Enter your code below
+
+
         }
     }
 }
